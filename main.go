e780aaf4f82d12bf52f98e7d11530a46265f53e1
umemak <umematsu@gmail.com>
2023-11-29 17:02:05 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	// ディレクトリの指定はコマンドライン引数で行う
+	// コマンドライン引数が指定されていない場合はカレントディレクトリを対象とする
+	dir := "." // カレントディレクトリを指定する
+	if len(os.Args) > 1 {
+		dir = os.Args[1]
+	}
+	ext := ".md" // 対象の拡張子を指定する
+
+	fileList := []string{}
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if strings.HasSuffix(path, ext) {
+			fileList = append(fileList, path)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	re := regexp.MustCompile(`productTitle:\s*(.+)`) // titleメタ情報を抽出する正規表現
+
+	for _, file := range fileList {
+		fmt.Printf("%q", file)
+		// ファイル名に" - "が含まれている場合は処理をスキップする
+		if strings.Contains(file, " - ") {
+			fmt.Println(" - skipped")
+			continue
+		}
+		content, err := os.ReadFile(file)
+		if err != nil {
+			fmt.Println(err)
+		}
+		match := re.FindStringSubmatch(string(content))
+		if len(match) > 1 {
+			title := strings.Trim(strings.TrimSpace(match[1]), "\"")
+			newName := fmt.Sprintf("%s - %s.md", strings.TrimSuffix(file, ext), title)
+			fmt.Printf(" -> %q", newName)
+			err = os.Rename(file, newName)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(" - renamed")
+		} else {
+			fmt.Println(" - skipped")
+		}
+	}
+}
